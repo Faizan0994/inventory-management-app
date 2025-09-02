5a818a0c3892b675fe1be903a178062052e29d92
faizan <faizanamjad575@gmail.com>
2025-09-02 13:18:27 +0500

    set up basic READ views with dummy data

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,3 +1,38 @@
+// dummy data
+const categories = [
+  {
+    name: "food",
+    id: 1,
+    items: [
+      {
+        item: "Bread",
+        cat: "food",
+        id: 1,
+        catId: 1,
+      },
+      {
+        item: "Biscuits",
+        cat: "food",
+        id: 2,
+        catId: 1,
+      },
+    ],
+  },
+  {
+    name: "stationery",
+    id: 2,
+    items: [
+      {
+        item: "Pens",
+        cat: "stationery",
+        id: 5,
+        catId: 2,
+      },
+    ],
+  },
+  { name: "sports", id: 3, items: [] },
+];
+
 exports.getCategories = (req, res) => {
-  res.send("A list of categories here");
+  res.render("categories", { categories: categories });
 };
--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,3 +1,25 @@
+// dummy data
+const items = [
+  {
+    item: "Bread",
+    cat: "food",
+    id: 1,
+    catId: 1,
+  },
+  {
+    item: "Biscuits",
+    cat: "food",
+    id: 2,
+    catId: 1,
+  },
+  {
+    item: "Pens",
+    cat: "stationery",
+    id: 5,
+    catId: 2,
+  },
+];
+
 exports.getItems = (req, res) => {
-  res.send("A list of items here");
+  res.render("items", { items: items });
 };
