ca255869421bbf77193566a2bcbdf58523e8886f
faizan <faizanamjad575@gmail.com>
2025-09-06 12:11:28 +0500

    remove unnecessary console log

--- database/pool.js
+++ database/pool.js
@@ -1,8 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config({ path: ".env" });
 
-console.log(process.env.localDB);
-
 const pool = new Pool({
   connectionString: process.env.localDB,
 });
